2db73408ffc2a1ebbde72fd93a2f78c623067bb6
Krispy King <kking620@gmail.com>
2025-08-31 17:38:05 -0700

    Second commit

--- main.cpp
+++ main.cpp
@@ -1,9 +1,12 @@
+//COMSC-210 | Lab 3 | Kristofer King
+//IDE used: VSC
 #include <iostream>
 
 using namespace std;
 
 int main()
 {
+   //outputs "Hello world!"
     cout << "Hello World!\n";
 
     return 0;
